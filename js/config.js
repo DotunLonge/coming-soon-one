10cd98f3c00d7efbd6c2967a0157f17925543209
Dotun <dotunlonge@Dotuns-MBP>
2018-06-10 13:20:04 +0100

    added demo

--- js/config.js
+++ js/config.js
@@ -1,14 +1,11 @@
 var showParticles = true,
-  mailChimpUrl =
-    "http://octrace.us14.list-manage.com/subscribe/post?u=b65c95763e0ae8e0c14e7af0e&amp;id=7e9cf89638",
-  setEmail = "octrace@gmail.com",
   showcountdown = false,
   // countdown setuo
   showCount = true,
   setDate = "2018/10/10",
   // background setup
   useOnlyColorForBackground = false,
-  backgroundColor = "pink",
+  backgroundColor = "green",
   staticBackground = false,
   staticBackgroundImage = "img/three.jpeg",
   dynamicBackgroundImages = [
--- js/index.js
+++ js/index.js
@@ -1,73 +1,121 @@
 $(() => {
-  //norm
-  $("#send-btn").css("background", backgroundColor);
-  $(".sp#configed").css("background", backgroundColor);
+  // Beginning Of Function Declarations
+  const colorRelated = function(backgroundColor) {
+    $("#top").css("background-color", backgroundColor);
+    $("#particles-js").css("backgroundColor", backgroundColor);
+    //norm
+    $("#send-btn").css("background", backgroundColor);
+    $(".sp#configed").css("background", backgroundColor);
+    $("#contact-us")
+      // .css("background", backgroundColor)
+      .css("color", backgroundColor);
 
-  $("#learn-more")
-    .mouseenter(function() {
-      $(this)
-        .css("background", backgroundColor)
-        .css("border-color", backgroundColor);
-    })
-    .mouseleave(function() {
-      $(this)
-        .css("background", "transparent")
-        .css("border-color", "white");
+    $("#learn-more")
+      .mouseenter(function() {
+        $(this)
+          .css("background", backgroundColor)
+          .css("border-color", backgroundColor);
+      })
+      .mouseleave(function() {
+        $(this)
+          .css("background", "transparent")
+          .css("border-color", "white");
+      });
+
+    $("#learn-more").on("click", function(e) {
+      var target = $("#about");
+      if (target.length) {
+        event.preventDefault();
+        $("html, body")
+          .stop()
+          .animate(
+            {
+              scrollTop: target.offset().top
+            },
+            1000
+          );
+      }
     });
 
-  $("#learn-more").on("click", function(e) {
-    var target = $("#bottom");
-    if (target.length) {
-      event.preventDefault();
-      $("html, body")
-        .stop()
-        .animate(
-          {
-            scrollTop: target.offset().top
-          },
-          1000
-        );
+    $("#contact-us")
+      .mouseenter(function() {
+        $(this)
+          .css("background", backgroundColor)
+          .css("border-color", backgroundColor)
+          .css("color", "white");
+      })
+      .mouseleave(function() {
+        $(this)
+          .css("background", "white")
+          .css("border-color", "white")
+          .css("color", backgroundColor);
+      });
+    $("#contact-us").on("click", function(e) {
+      var target = $("#contact");
+      if (target.length) {
+        event.preventDefault();
+        $("html, body")
+          .stop()
+          .animate(
+            {
+              scrollTop: target.offset().top
+            },
+            1000
+          );
+      }
+    });
+
+    $("#send-btn")
+      .mouseleave(function() {
+        $(this)
+          .css("background", backgroundColor)
+          .css("color", "white");
+      })
+      .mouseenter(function() {
+        $(this)
+          .css("background", "white")
+          .css("color", backgroundColor);
+      });
+  };
+  const toggleCount = function(showCount) {
+    if (showCount) {
+      $("#date-time").countdown(setDate, function(event) {
+        $(this).html(`
+          <div class="xs-3">
+              <h3 id='days'>${event.strftime("%D")}</h3>
+                  <p>Days</p>
+              </div>
+              <div class="xs-3">
+                  <h3 id='hrs'>${event.strftime("%H")}</h3>
+                  <p>Hours</p>
+              </div>
+              <div class="xs-3">
+                  <h3 id='mins'>${event.strftime("%M")}</h3>
+                  <p>Minutes</p>
+              </div>
+              <div class="xs-3">
+                  <h3 id='secs'>${event.strftime("%S")}</h3>
+                  <p>Seconds</p>
+              </div>
+          `);
+      });
+    } else {
+      $("#date-time").countdown("pause");
+      $("#date-time").html("");
     }
-  });
+  };
+  // End Of Function Declarations
 
-  $("#send-btn")
-    .mouseleave(function() {
-      $(this)
-        .css("background", backgroundColor)
-        .css("color", "white");
-    })
-    .mouseenter(function() {
-      $(this)
-        .css("background", "white")
-        .css("color", backgroundColor);
-    });
+  // Beginning Of Function Calls
+  colorRelated(backgroundColor);
+  toggleCount(backgroundColor);
+  // End Of Function Calls
 
-  if (showCount) {
-    $("#date-time").countdown(setDate, function(event) {
-      $(this).html(`
-        <div class="xs-3">
-            <h3 id='days'>${event.strftime("%D")}</h3>
-                <p>Days</p>
-            </div>
-            <div class="xs-3">
-                <h3 id='hrs'>${event.strftime("%H")}</h3>
-                <p>Hours</p>
-            </div>
-            <div class="xs-3">
-                <h3 id='mins'>${event.strftime("%M")}</h3>
-                <p>Minutes</p>
-            </div>
-            <div class="xs-3">
-                <h3 id='secs'>${event.strftime("%S")}</h3>
-                <p>Seconds</p>
-            </div>
-        `);
-    });
+  // Beginning Of Conditionals
+  if (showParticles) {
+    particlesJS.load("particles-js", "./js/particles/config.json");
   }
-
-  if (useOnlyColorForBackground) {
-    $("#top").css("backgroundColor", backgroundColor);
-  } else {
+  if (useOnlyColorForBackground === false) {
     if (staticBackground) {
       $("#top").backstretch(staticBackgroundImage);
     } else {
@@ -76,10 +124,25 @@ $(() => {
         fade: transitionFade
       });
     }
-    $("#particles-js").css("backgroundColor", backgroundColor);
   }
-  //conditions
-  if (showParticles) {
-    particlesJS.load("particles-js", "./js/particles/config.json");
-  }
+  // End Of Conditionals
+
+  // Beginning Of Listeners
+  $("#color-picker").on("change", function(e) {
+    colorRelated(e.target.value);
+  });
+  $("#true").on("click", function(e) {
+    toggleCount(true);
+    $(this).addClass("active");
+    $("#false").removeClass("active");
+  });
+  $("#false").on("click", function(e) {
+    toggleCount(false);
+    $(this).addClass("active");
+    $("#true").removeClass("active");
+  });
+  $("#cog").on("click", function(e) {
+    $(".sidebar").toggleClass("hide-away");
+  });
+  // End Of Listeners
 });
